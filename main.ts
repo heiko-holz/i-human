f6ed5d0a363a98fee12beced22e071bd91656a3a
Heiko Holz <heiko@whysoseriousgames.de>
2024-04-10 15:40:02 +0200

    initial commit

--- main.ts
+++ main.ts
@@ -1,9 +1,21 @@
+let beat = 0
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    beat += 1
+})
 basic.forever(function () {
-    basic.showLeds(`
-        . # . # .
-        # # # # #
-        # # # # #
-        . # # # .
-        . . # . .
-        `)
+    if (beat >= 2) {
+        for (let index = 0; index <= 3; index++) {
+            basic.showLeds(`
+                . # . # .
+                # # # # #
+                # # # # #
+                . # # # .
+                . . # . .
+                `)
+            basic.pause(500)
+            basic.clearScreen()
+            basic.pause(500)
+        }
+        beat = 0
+    }
 })
