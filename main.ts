1a3e66e24bdf2f7a929f7bbbe5a4733de2d7be44
Heiko Holz <heiko@whysoseriousgames.de>
2024-04-10 17:29:02 +0200

    MarAI.ke lives

--- main.ts
+++ main.ts
@@ -1,34 +1,30 @@
 let heart = 0
+let time = 0
+let elapsed = 0
+let pause2 = 0
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     heart += 1
 })
 basic.forever(function () {
     if (heart >= 2) {
-        if (true) {
+        if (false) {
             basic.showString("CPR active for 10 seconds")
         }
-        for (let index = 0; index <= 8; index++) {
-            basic.showLeds(`
-                . # . # .
-                # # # # #
-                # # # # #
-                . # # # .
-                . . # . .
-                `)
-            music.playTone(494, music.beat(BeatFraction.Whole))
-            basic.pause(555)
-            basic.clearScreen()
-            basic.pause(555)
+        for (let index = 0; index < 9; index++) {
+            time = input.runningTime()
+            basic.showIcon(IconNames.Heart)
+            music.playTone(494, music.beat(BeatFraction.Half))
+            elapsed = input.runningTime() - time
+            pause2 = 555 - elapsed
+            basic.pause(pause2)
+            time = input.runningTime()
+            basic.showIcon(IconNames.SmallHeart)
+            elapsed = input.runningTime() - time
+            pause2 = 555 - elapsed
+            basic.pause(pause2)
         }
-        basic.showLeds(`
-            . . . . .
-            . # . . .
-            # . # . #
-            . . . # .
-            . . . . .
-            `)
+        basic.showIcon(IconNames.StickFigure, 1000)
         music.playMelody("E D G F B A C5 B ", 389)
-        basic.pause(1000)
         basic.clearScreen()
         heart = 0
     }
